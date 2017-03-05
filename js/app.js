615be97e133536453e9b5cbb3eb284485c4350ec
Ross King <sleeping.stars@gmail.com>
2017-03-05 13:00:31 +0000

    Moved nav to top of screen, updated shop links

--- js/app.js
+++ js/app.js
@@ -13,6 +13,41 @@ $(function(){
           var height = $('#hero').height();
           $('.fullscreen').css('height', height);
         }
+
+        // Smooth Scroll
+        $('a[href*="#"]:not([href="#"])').click(function() {
+          if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+              var menuHeight = $('nav').height();
+              $('html, body').animate({
+                scrollTop: target.offset().top - menuHeight
+              }, 1000);
+              return false;
+            }
+          }
+        });
+
+        // Apply Header Style After Hero
+        $(window).scroll(function() {
+          var scroll = $(window).scrollTop();
+          var $hero = $('#hero');
+          var heroBottom = $hero.position().top + $hero.outerHeight(true);
+
+          if (scroll >= heroBottom) {
+            $("nav").addClass("fixed");
+          } else {
+            $("nav").removeClass("fixed");
+          }
+        });
+
+
+
+
+
+
+
       }
     };
 
