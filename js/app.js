7fc1c3fc8762786bb3061989caaed134017998ea
Ross King <sleeping.stars@gmail.com>
2017-03-05 14:15:25 +0000

    Added code for form functionality

--- js/app.js
+++ js/app.js
@@ -43,8 +43,61 @@ $(function(){
           }
         });
 
+        // Form Code
+        // Get the form.
+      	var form = $('#ajax-contact');
 
+      	// Get the messages div.
+      	var formMessages = $('#form-messages');
 
+      	// Set up an event listener for the contact form.
+      	$(form).submit(function(e) {
+      		// Stop the browser from submitting the form.
+      		e.preventDefault();
+
+          $.ajax({
+        		url: '//formspree.io/sleeping.stars@gmail.com',
+        		method: 'POST',
+        		data: $(this).serialize(),
+        		dataType: 'json',
+            success: function(response) {
+        			// Make sure that the formMessages div has the 'success' class.
+        			$(formMessages).removeClass('error');
+        			$(formMessages).addClass('success');
+
+        			// Set the message text.
+        			$(formMessages).text("Message sent!");
+
+              setTimeout(function(){
+                $(formMessages).removeClass('success');
+                $(formMessages).text("");
+              }, 6000);
+
+        			// Clear the form.
+        			$('#name').val('');
+        			$('#email').val('');
+        			$('#message').val('');
+        		},
+        		error: function(data) {
+        			// Make sure that the formMessages div has the 'error' class.
+        			$(formMessages).removeClass('success');
+        			$(formMessages).addClass('error');
+
+        			// Set the message text.
+        			if (data.responseText !== '') {
+        				$(formMessages).text("Oops, there was an error.");
+        			} else {
+        				$(formMessages).text('Oops! An error occured and your message could not be sent.');
+        			}
+
+              setTimeout(function(){
+                $(formMessages).removeClass('error');
+                $(formMessages).text("");
+              }, 6000);
+        	   }
+           });
+      	});
+        // Form Code End
 
 
 
